44e2b77fcc0ef9c87286432a0afbe0c5331d9f0b
Sebas Sarmiento <jssarmiento@sudamericano.edu.ec>
2026-02-27 00:57:47 -0500

    IB

---
+++ src/data/platos.js
@@ -0,0 +1,20 @@
+export const platos = [
+    { 
+        nombre: 'Pelúa', 
+        descripcion: 'Carne mechada con queso amarillo', 
+        precio: '5.00', 
+        imagen: '/pelua.jpg' 
+    },
+    { 
+        nombre: 'Rompe colchón', 
+        descripcion: 'Mezcla de mariscos frescos', 
+        precio: '5.00', 
+        imagen: '/rompe.jpg' 
+    },
+    { 
+        nombre: 'Gringa', 
+        descripcion: 'Carne asada, queso y aguacate', 
+        precio: '6.50', 
+        imagen: '/gringa.jpg' 
+    }
+];
